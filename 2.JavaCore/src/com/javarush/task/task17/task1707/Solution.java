268487669f11f8defae8102cac4e67b6a6f650d2
Vadym Pylypchenko <vadym_pylyp@ukr.net>
2017-06-25 18:46:31 +0300

    test

--- 2.JavaCore/src/com/javarush/task/task17/task1707/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1707/Solution.java
@@ -6,7 +6,7 @@ package com.javarush.task.task17.task1707;
 
 public class Solution {
     public static void main(String[] args) {
-        IMF fund = IMF.getFund();
+        IMF fund = IMF.getFund() ;
         IMF anotherFund = IMF.getFund();
         System.out.println(fund == anotherFund );
     }
